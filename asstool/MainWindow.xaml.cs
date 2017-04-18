78859656f96e5e85d0e9917162d5d20931974d46
xue-blood <xue.blood@outlook.com>
2017-04-18 09:00:37 +0800

    install mahapp pacages

---
+++ asstool/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MahApps.Metro;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Setting = asstool.Properties.Settings;
+
+namespace asstool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            set_theme_and_accent();
+        }
+
+        void set_theme_and_accent()
+        {
+            // set accent 
+            var accent = ThemeManager.GetAccent(Setting.Default.color);
+            var theme = ThemeManager.GetAppTheme(Setting.Default.theme);
+            if(accent != null && theme != null)
+            {
+                ThemeManager.ChangeAppStyle(Application.Current, accent, theme);
+            }
+        }
+    }
+}
